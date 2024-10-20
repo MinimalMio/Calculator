9f2bb25ed25d8da2ba6f5e695b332c172450fce0
MinimalMio <yukati2024@outlook.com>
2024-10-20 19:42:05 +0800

    [feat] more more more more....

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,159 @@
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <map>
+#include <stack>
+#include <stdexcept>
+#include <cctype>
+#include <cmath>
 
 #include "include/calc.hpp"
 
-using namespace std;
+std::map<std::string, double> variables;
 
-int loopValue = 0; // used for while
-string command;    // used for command shell
+void error(const std::string &msg) {
+    std::cout << "[ERR] " << msg << std::endl;
+}
 
-int main() {
-    while ( loopValue == 0 ) {
-        cout << "Calculator v" << version << endl;
-        while ( loopValue == 0 ) {
-            cout << ">>> ";
-            cin >> command;
-            if ( command == "version" ) {
-                cout << "Calculator v" << version << endl;
-            } else if ( command == "exit" ) {
-                cout << "Good bye." << endl;
-                return 0;
-            } else if ( command == "help" ) {
-                cout << "writing..." << endl;
-            } else {
-                cout << "Unknown command." << endl;
-            }
+double evaluateExpression(std::istringstream &iss);
+
+double evaluateTerm(std::istringstream &iss);
+
+double evaluateFactor(std::istringstream &iss);
+
+double evaluateExpression(std::istringstream &iss) {
+    double result = evaluateTerm(iss);
+    while (iss) {
+        char op;
+        iss >> op;
+        if (op == '+' || op == '-') {
+            double nextTerm = evaluateTerm(iss);
+            if (op == '+') result += nextTerm;
+            else result -= nextTerm;
+        } else {
+            iss.putback(op);
+            break;
         }
     }
+    return result;
+}
+
+double evaluateTerm(std::istringstream &iss) {
+    double result = evaluateFactor(iss);
+    while (iss) {
+        char op;
+        iss >> op;
+        if (op == '*' || op == '/') {
+            double nextFactor = evaluateFactor(iss);
+            if (op == '*') result *= nextFactor;
+            else if (nextFactor == 0) throw std::runtime_error("Divider cannot be zero.");
+            else result /= nextFactor;
+        } else {
+            iss.putback(op);
+            break;
+        }
+    }
+    return result;
+}
+
+double evaluateFactor(std::istringstream &iss) {
+    double result;
+    char c;
+    iss >> c;
+    if (c == '(') {
+        result = evaluateExpression(iss);
+        iss >> c;
+    } else if (isalpha(c)) {
+        std::string var;
+        iss.putback(c);
+        iss >> var;
+        if (variables.find(var) != variables.end()) {
+            result = variables[var];
+        } else {
+            throw std::runtime_error("Unknown variable: " + var);
+        }
+    } else {
+        iss.putback(c);
+        iss >> result;
+    }
+    return result;
+}
+
+void handleLetCommand(const std::string &line) {
+    std::istringstream iss(line);
+    std::string command, varName, equalSign;
+    iss >> command >> varName >> equalSign;
+    if (equalSign != "=") {
+        error("Invalid let syntax");
+        return;
+    }
+    double value = evaluateExpression(iss);
+    variables[varName] = value;
+}
+
+void handleShowCommand(const std::string &varName) {
+    if (variables.find(varName) != variables.end()) {
+        std::cout << variables[varName] << std::endl;
+    } else {
+        error("Unknown variable: " + varName);
+    }
+}
+
+void handleDelCommand(const std::string &varName) {
+    if (variables.erase(varName) == 0) {
+        error("Unknown variable: " + varName);
+    }
+}
+
+void showHelp() {
+    std::cout << "Calculator v" << version << std::endl;
+    std::cout << "let : define a variable" << std::endl;
+    std::cout << "show: print value of a variable" << std::endl;
+    std::cout << "del : del a variable" << std::endl;
+    std::cout << "help: show this message" << std::endl;
+    std::cout << "ver : display version" << std::endl;
+    std::cout << "exit: exit\n";
+}
+
+void showVersion() {
+    std::cout << "Calculator v" << version << std::endl;
+}
+
+int main() {
+    std::cout << "Calculator v" << version << std::endl;
+    std::string line;
+    std::cout << ">>> ";
+    while (std::getline(std::cin, line)) {
+        try {
+            std::istringstream iss(line);
+            std::string command;
+            iss >> command;
+            if (command == "let") {
+                handleLetCommand(line);
+            } else if (command == "show") {
+                std::string varName;
+                iss >> varName;
+                handleShowCommand(varName);
+            } else if (command == "del") {
+                std::string varName;
+                iss >> varName;
+                handleDelCommand(varName);
+            } else if (command == "help") {
+                showHelp();
+            } else if (command == "ver") {
+                showVersion();
+            } else if (command == "exit") {
+                break;
+            } else {
+                iss.clear();
+                iss.str(line);
+                double result = evaluateExpression(iss);
+                std::cout << result << std::endl;
+            }
+        } catch (std::exception &e) {
+            error(e.what());
+        }
+        std::cout << ">>> ";
+    }
+    return 0;
 }
